011be7deee582269b796d573099470afaf18dd05
Pranav Narawade <pranav.narawade@bhn.com>
2025-07-06 23:38:30 +0530

    Minor change in naming convention

--- orchestrator/apis/api/getAllProductsInfo.js
+++ orchestrator/apis/api/getAllProductsInfo.js
@@ -1,10 +1,10 @@
 import { GetAllProducts } from "../../commerce/commerceFunctions/getAllProducts.js";
 
 export const getAllProductsInfo = (async (req, res) => {
-    const inputdata = req.body?.input ?? null;
+    const inputData = req.body?.input ?? null;
     try {
         const getAllProducts = new GetAllProducts();
-        const response = await getAllProducts.getProducts(inputdata);
+        const response = await getAllProducts.getProducts(inputData);
         return res.status(200).json(response);
     } catch (error) {
         return res.status(400).json({ error: error.message });
